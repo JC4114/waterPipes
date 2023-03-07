94025b77fadec47125415c4af5d9b65b046a8212
JC4114 <4114114jc@gmail.com>
2023-03-07 14:13:30 +0200

    live

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-import { useState,useEffect } from 'react';
+
+import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
 
 function water(arr) {
   let count = 0;
@@ -21,76 +23,49 @@ function water(arr) {
 }
 
 
- 
-
-
 export const App = () => {
+  const [inputValue, setInputValue] = useState([]);
+  const [pipes, setPipes] = useState([]);
+  const [waterCount, setWaterCount] = useState(0);
 
-  const getRandomColor = () =>
-    `rgb(${Math.round(Math.random() * (255 - 0) + 1)}, ${Math.round(
-      Math.random() * (255 - 0) + 1
-    )}, ${Math.round(Math.random() * (255 - 0) + 1)})`;
-
-  
-  
-    const [inputValue, setInputValue] = useState([]);
-    const [pipes, setPipes] = useState([]);
-  
-    useEffect(() => {
-      setPipes(inputValue.filter(Boolean).map(pipe => parseInt(pipe.trim(), 10)));
-    }, [inputValue]);
-  
-    const handleInputChange = (event) => {
-      const value = event.target.value;
-      const values = value.split(',').map(v => v.trim()); // убираем пробелы вокруг значений
-      setInputValue(values);
-    };
-  
-    return (
-      <div>
-          <input type="text" value={inputValue} onChange={handleInputChange} />
-          <div style={{ display: 'flex', flexDirection: 'wrap-reverse', flexWrap: 'wrap' }}>
-  {pipes.map((pipe) => (
-    <div key={getRandomColor()} style={{ backgroundColor: 'green', width: '50px', border: '1px dashed red', height: `${20 * pipe}px` }}>
-      {pipe}
-    </div>
-  ))}
-</div>
-
-      </div>
+  useEffect(() => {
+    setPipes(
+      inputValue
+        .filter(Boolean)
+        .map((pipe) => parseInt(pipe.trim(), 10))
     );
+  }, [inputValue]);
+
+  useEffect(() => {
+    setWaterCount((prevCount) => water(pipes));
+  }, [pipes]);
+
+  const handleInputChange = (event) => {
+  const value = event.target.value;
+  const values = value.split(',').map(v => v.trim()); // убираем пробелы вокруг значений
+  setInputValue(values);
   };
-  
 
-// const [inputValue, setInputValue] = useState('');
-//   const [pipes, setPipes] = useState([]);
-
-//   useEffect(() => {
-//     setPipes(inputValue.map(pipe => Number(pipe))); // обновляем состояние pipes при изменении inputValue
-//   }, [inputValue]);
-
-//   const handleInputChange = (event) => {
-//     const value = event.target.value;
-//     const values = value.split(',').map(v => v.trim()); // убираем пробелы вокруг значений
-//     setInputValue(values);
-//   };
-
-//   return (
-//     <div>
-//       <form>
-//         <input type="text" value={inputValue} onChange={handleInputChange} />
-//       </form>
-//       <div>
-//         {pipes.map((pipe) => (
-//           <div key={getRandomColor()} style={{ backgroundColor: 'green', width: '50px', border: '1px dashed red', height: `${20 * pipe}px` }}>
-//             {pipe}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-
-
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+        {pipes.map((pipe) => (
+          <div
+            key={nanoid(4)}
+            style={{
+              backgroundColor: 'green',
+              width: '50px',
+              border: '1px dashed red',
+              height: `${20 * pipe}px`,
+            }}
+          >
+            {pipe}
+          </div>
+        ))}
+      </div>
+      <div>Колличество воды:  {waterCount}</div>
+    </div>
+  );
+};
 
